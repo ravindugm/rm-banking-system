4633462f575c65e31fb456c61917dc465e3f2874
ravindugm <ravindumiyuranga@gmail.com>
2021-07-10 23:30:11 +0530

    Initial Commit

---
+++ src/main/java/com/springboot/rmbankingsystem/RmBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.rmbankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmBankingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/rmbankingsystem/RmBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.rmbankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

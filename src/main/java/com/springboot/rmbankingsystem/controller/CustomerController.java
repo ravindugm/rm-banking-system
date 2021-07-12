d8ebbce2e061016245f30f6dcfa16c11535b33e2
ravindugm <ravindumiyuranga@gmail.com>
2021-07-13 01:20:37 +0530

    Add Customer REST controller

--- src/main/java/com/springboot/rmbankingsystem/controller/CustomerController.java
+++ src/main/java/com/springboot/rmbankingsystem/controller/CustomerController.java
@@ -3,15 +3,12 @@ package com.springboot.rmbankingsystem.controller;
 import com.springboot.rmbankingsystem.model.Customer;
 import com.springboot.rmbankingsystem.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/bank")
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bank")
 public class CustomerController {
 
     private CustomerService customerService;
@@ -21,24 +18,49 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping("/customerRegistrationForm")
-    public String showCustomerRegistrationForm(Model theModel) {
-
-        Customer theCustomer = new Customer();
-
-        theModel.addAttribute("customer", theCustomer);
-
-        return "customer/customer-registration-form";
+    @GetMapping("/customers")
+    public List<Customer> findAll() {
+        return customerService.findAll();
     }
 
-    @PostMapping("/registerCustomer")
-    public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+    @GetMapping("/customers/{customerId}")
+    public Customer findById(@PathVariable int customerId) {
+        Customer customer = customerService.findById(customerId);
+
+        if (customer == null) {
+            throw new RuntimeException("Customer of id " + customerId + " is not found");
+        }
+
+        return customer;
+    }
+
+    @PostMapping("/customers")
+    public Customer saveCustomer(@RequestBody Customer theCustomer) {
+        theCustomer.setId(0);
+
         customerService.saveCustomer(theCustomer);
-        return "redirect:/bank/showCustomerRegistrationSuccessful";
+
+        return theCustomer;
     }
 
-    @GetMapping("/showCustomerRegistrationSuccessful")
-    public String showEmployeeRegistrationSuccessfulForm() {
-        return "customer/customer-registration-complete-form";
+    @PutMapping("/customers")
+    public Customer updateCustomer(@RequestBody Customer thwCustomer) {
+        customerService.saveCustomer(thwCustomer);
+
+        return thwCustomer;
     }
+
+    @DeleteMapping("/customers/{customerId}")
+    public String deleteCustomer(@PathVariable int customerId) {
+        Customer theCustomer = customerService.findById(customerId);
+
+        if (theCustomer == null) {
+            throw new RuntimeException("Customer of id " + customerId + " not found");
+        }
+
+        customerService.deleteById(customerId);
+
+        return "Delete the record of id: " + customerId;
+    }
+
 }

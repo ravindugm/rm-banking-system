107aede709049c7f04e0fa4bb4360f0ff8be4941
ravindugm <ravindumiyuranga@gmail.com>
2021-07-13 00:25:35 +0530

    Add Customer service IMPl file

--- src/main/java/com/springboot/rmbankingsystem/service/CustomerServiceImpl.java
+++ src/main/java/com/springboot/rmbankingsystem/service/CustomerServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
@@ -19,12 +20,22 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public List<Customer> findAll() {
-        return null;
+        return customerRepository.findAll();
     }
 
     @Override
     public Customer findById(int theId) {
-        return null;
+        Optional<Customer> results = customerRepository.findById(theId);
+
+        Customer theCustomer = null;
+
+        if (results.isPresent()) {
+            theCustomer = results.get();
+        } else {
+            throw new RuntimeException("Did not find customer id:" + theId);
+        }
+
+        return theCustomer;
     }
 
     @Override
@@ -34,6 +45,6 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public void deleteById(int theId) {
-
+        customerRepository.deleteById(theId);
     }
 }
